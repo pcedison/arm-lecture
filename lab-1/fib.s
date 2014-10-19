84f856a93d04b5052aaeb0d287b1439b7067ab6c
Marcus <pcedison@gmail.com>
2014-10-19 14:32:42 +0800

    Remove the old version code

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -38,37 +38,9 @@ fibonacci:
         add r0, r0, r5
         pop {r4, r5, pc}
 
-<<<<<<< HEAD
-
-=======
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-        subs r4, r0, #0
-        ble .L3
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-        cmp r4, #1
-        beq .L4
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-        sub r0, r4, #1
-        bl fibonacci
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-        mov r5,r0
-        sub r0, r4, #2
-        bl fibonacci
-
-	@ R0 = R5 + R0 (update flags)
-        adds r0, r5, r0
-	pop {r3, r4, r5, pc}		@EPILOG
->>>>>>> 49f4dde90b9f64d1ca54bc87ebce80eb04cffaae
 
 	@ END CODE MODIFICATION
+	@ EPILOG
 .L3:
 	mov r0, #0			@ R0 = 0
 	pop {r4, r5, pc}		@ EPILOG
